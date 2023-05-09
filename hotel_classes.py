966c38cbfa51d0e5be612b79fdc68d0e5be0a2cb
Haani <116673121+haanisyed@users.noreply.github.com>
2023-05-08 21:47:44 -0400

    Update hotel_classes.py

--- hotel_classes.py
+++ hotel_classes.py
@@ -1,13 +1,7 @@
 """
    CISC-121 2023W
-
    Name: Haani Syed
-   Student Number: 20331181
-   Email: 21ahs7@queensu.ca
    Date: 2023-03-30
-
-   I confirm that this assignment solution is my own work and conforms to
-   Queen's standards of Academic Integrity
 """
 
 
